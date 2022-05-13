57f16f140441812bfe36e1fb5f17ecaf6728833f
wick <wickham.eaw@gmail.com>
2022-05-12 20:02:37 -0400

    initial commit

---
+++ packages/wick-component-library/rollup.config.js
@@ -0,0 +1,30 @@
+const {nodeResolve} = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('rollup-plugin-typescript2');
+const postCSS = require('rollup-plugin-postcss');
+const pkg = require('./package.json');
+
+module.exports = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: './lib/cjs/index.js',
+      format: 'cjs',
+    },
+    {
+      file: './lib/esm/index.js',
+      format: 'es',
+    },
+  ],
+  external: [...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      typescript: require('typescript'),
+    }),
+    postCSS({
+      plugins: [require('autoprefixer')],
+    }),
+  ],
+};
